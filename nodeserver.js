89f48339435f8eaf9ae3a7e93b480c8443c26dab
SHIVA <balachandarsivakumar@gmail.com>
2019-11-07 11:18:53 +0530

    Initial Deploy

--- nodeserver.js
+++ nodeserver.js
@@ -4,15 +4,18 @@ const app = express();
 const path = require('path');
 
 // Directory Name dist file folder //
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/MCQ'));
 
 // Listening Process Function //
 app.listen(process.env.PORT || 8080);
 
 //Get Function for sendfile strategy//
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+//Get Application for directory name access//
+    res.sendFile(path.join(__dirname + '/dist/MCQ/index.html'));
 })
 
 //Heroku Port listening //
 console.log('console listening!');
+
+// After function listening for Heroku master
