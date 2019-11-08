9805391030b10d55264238e1adb369760679cf33
SHIVA <balachandarsivakumar@gmail.com>
2019-11-08 12:02:41 +0530

    Intial Deploy Updates

--- nodeserver.js
+++ nodeserver.js
@@ -4,7 +4,7 @@ const app = express();
 const path = require('path');
 
 // Directory Name dist file folder //
-app.use(express.static(__dirname + '/dist/onlineassessmenttest'));
+app.use(express.static(__dirname + '/dist/MCQ'));
 
 // Listening Process Function //
 app.listen(process.env.PORT || 8080);
@@ -13,10 +13,9 @@ app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
 
 //Get Application for directory name access//
-    res.sendFile(path.join(__dirname + '/dist/onlineassessmenttest/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/MCQ/index.html'));
 })
 
 //Heroku Port listening //
 console.log('console listening!');
 
-// After function listening for Heroku master
